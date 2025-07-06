dbcf721ac7a743aa01fe7b261493b988249116ec
bugkay101 <149082141+bugkay101@users.noreply.github.com>
2025-07-06 19:30:27 +0100

    finish auth

--- components/providers/Web3Provider.tsx
+++ components/providers/Web3Provider.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@rainbow-me/rainbowkit/styles.css";
 import {
+  AuthenticationStatus,
   connectorsForWallets,
+  createAuthenticationAdapter,
   RainbowKitAuthenticationProvider,
   RainbowKitProvider,
 } from "@rainbow-me/rainbowkit";
@@ -14,6 +16,7 @@ import {
 import { createConfig, http, WagmiProvider } from "wagmi";
 import { sepolia } from "wagmi/chains";
 import { authenticationAdapter } from "@/lib/auth-adapter";
+import { createSiweMessage } from "viem/siwe";
 
 // Create connectors without WalletConnect to avoid ES module conflicts
 const connectors = connectorsForWallets(
@@ -43,7 +46,6 @@ interface Web3ProviderProps {
 }
 
 export function Web3Provider({ children }: Web3ProviderProps) {
-  const AUTHENTICATION_STATUS ='unauthenticated'; //'loading' | "authenticated"
   // Create a new QueryClient instance for each user session
   const [queryClient] = useState(
     () =>
@@ -57,12 +59,95 @@ export function Web3Provider({ children }: Web3ProviderProps) {
       })
   );
 
+    const fetchingStatusRef = useRef(false);
+  const verifyingRef = useRef(false);
+  const [authStatus, setAuthStatus] = useState<AuthenticationStatus>('loading');
+
+  // Fetch user when:
+  useEffect(() => {
+    const fetchStatus = async () => {
+      if (fetchingStatusRef.current || verifyingRef.current) {
+        return;
+      }
+
+      fetchingStatusRef.current = true;
+
+      try {
+        const response = await fetch('/api/auth/me');
+        const json = await response.json();
+        setAuthStatus(json.address ? 'authenticated' : 'unauthenticated');
+      } catch (_error) {
+        setAuthStatus('unauthenticated');
+      } finally {
+        fetchingStatusRef.current = false;
+      }
+    };
+
+    // 1. page loads
+    fetchStatus();
+
+    // 2. window is focused (in case user logs out of another window)
+    window.addEventListener('focus', fetchStatus);
+    return () => window.removeEventListener('focus', fetchStatus);
+  }, []);
+
+  const authAdapter = useMemo(() => {
+    return createAuthenticationAdapter({
+      getNonce: async () => {
+        const response = await fetch('/api/auth/nonce');
+        return await response.text();
+      },
+
+      createMessage: ({ nonce, address, chainId }) => {
+        return createSiweMessage({
+          domain: window.location.host,
+          address,
+          statement: 'Sign in with Ethereum to the app.',
+          uri: window.location.origin,
+          version: '1',
+          chainId,
+          nonce,
+        });
+      },
+
+      verify: async ({ message, signature }) => {
+        verifyingRef.current = true;
+
+        try {
+          const response = await fetch('/api/auth/verify', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ message, signature }),
+          });
+
+          const authenticated = Boolean(response.ok);
+
+          if (authenticated) {
+            setAuthStatus(authenticated ? 'authenticated' : 'unauthenticated');
+          }
+
+          return authenticated;
+        } catch (error) {
+          console.error('Error verifying signature', error);
+          return false;
+        } finally {
+          verifyingRef.current = false;
+        }
+      },
+
+      signOut: async () => {
+        setAuthStatus('unauthenticated');
+        await fetch('/api/auth/logout');
+      },
+    });
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <WagmiProvider config={config}>
         <RainbowKitAuthenticationProvider
-          adapter={authenticationAdapter}
-          status={AUTHENTICATION_STATUS}
+          adapter={authAdapter}
+          status={authStatus}
         >
           <RainbowKitProvider>{children}</RainbowKitProvider>
         </RainbowKitAuthenticationProvider>
